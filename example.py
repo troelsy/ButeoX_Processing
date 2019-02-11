231d53d4308db09e230b6500fb7723381ff058e8
Troels Ynddal <troelsynddal@gmail.com>
2019-02-11 10:53:57 +0100

    Use adaptive_median to remove noise before downsampling

--- example.py
+++ example.py
@@ -1,12 +1,13 @@
 import pickle
 
-from utils import show, save, calibrate, interpolate
+from utils import show, save, calibrate, interpolate, adaptive_median
 
 
 with open("potato.pickle", "rb") as f:
     image = pickle.load(f, encoding="latin1")
 
-image = calibrate(image)
-image = interpolate(image, 3, mode="spline")
+image = calibrate(image)  # Compensate for fixed-pattern noise
+image = adaptive_median(image)  # Reduce gaussian noise and remove impulses
+image = interpolate(image, 3, mode="median")  # downsample to fit 'normal' y-scale
 
-show(image, r=None)
+show(image, r=(0, 1.2))
